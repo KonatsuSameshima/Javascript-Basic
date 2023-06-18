788a76bf197c9b89dab63f9a7f1e70d1587205ce
KonatsuSameshima <136305295+KonatsuSameshima@users.noreply.github.com>
2023-06-19 00:26:51 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+//体重
+let bodyWeight;
+bodyWeight = 'kg';
+
+//身長
+let bodyHeight;
+bodyHeight = 'm';
+
+//変数の代入
+bodyWeight = 68;
+bodyHeight = 1.7;
+
+//計算式
+console.log(bodyWeight / (bodyHeight * bodyHeight) );
